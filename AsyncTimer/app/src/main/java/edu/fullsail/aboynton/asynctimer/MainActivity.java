5329a9e32a68b640174fea1dd9bfdee917d2973a
Allen Boynton <aboynton@fullsail.edu>
2017-02-20 01:06:07 -0500

    Label my MainActivity

--- AsyncTimer/app/src/main/java/edu/fullsail/aboynton/asynctimer/MainActivity.java
+++ AsyncTimer/app/src/main/java/edu/fullsail/aboynton/asynctimer/MainActivity.java
@@ -1,9 +1,20 @@
+// Allen Boynton
+
+// JAV1 - 1702
+
+// MainActivity.java
+
 package edu.fullsail.aboynton.asynctimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /*TODO: Spent all my time on Google Books, never worked,
+     never got to this one and did not even get to the test*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
